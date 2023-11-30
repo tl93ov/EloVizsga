51e61b52b922073e684c8507d373a549389b7b06
tl93ov <tl93ov@neptun.gde.hu>
2023-11-30 17:12:52 +0100

    init

---
+++ Classes.py
@@ -0,0 +1,71 @@
+from abc import ABC, abstractmethod
+from datetime import datetime, timedelta
+
+class Szoba(ABC):
+    def __init__(self, szobaszam, ar):
+        self.szobaszam = szobaszam
+        self.ar = ar
+
+    @abstractmethod
+    def get_tipus(self):
+        pass
+
+class EgyagyasSzoba(Szoba):
+    def __init__(self, szobaszam):
+        super().__init__(szobaszam, 10000)
+
+    def get_tipus(self):
+        return "Egyágyas szoba"
+
+class KetagyasSzoba(Szoba):
+    def __init__(self, szobaszam):
+        super().__init__(szobaszam, 15000)
+
+    def get_tipus(self):
+        return "Kétágyas szoba"
+
+class Szalloda:
+    def __init__(self, nev):
+        self.nev = nev
+        self.szobak = []
+
+    def add_szoba(self, szoba):
+        self.szobak.append(szoba)
+
+class Foglalas:
+    def __init__(self, szoba, datum):
+        self.szoba = szoba
+        self.datum = datum
+
+class FoglalasKezelo:
+    def __init__(self):
+        self.foglalasok = []
+
+    def foglalas(self, szoba, datum):
+        today = datetime.today()
+        today = today.replace(hour=0, minute=0, second=0, microsecond=0)
+        if datum < today:
+            print("Érvénytelen dátum, csak jövőbeni foglalásokat fogadunk el.")
+            return
+
+        for foglalas in self.foglalasok:
+            if foglalas.szoba == szoba and foglalas.datum == datum:
+                print("A szoba már foglalt ezen a napon.")
+                return
+
+        self.foglalasok.append(Foglalas(szoba, datum))
+        print("Sikeres foglalás.")
+
+    def lemondas(self, szoba, datum):
+        for foglalas in self.foglalasok:
+            if foglalas.szoba == szoba and foglalas.datum == datum:
+                self.foglalasok.remove(foglalas)
+                print("Sikeres lemondás.")
+                return
+
+        print("Nincs ilyen foglalás.")
+
+    def listazas(self):
+        print("Foglalások:")
+        for foglalas in self.foglalasok:
+            print(f"{foglalas.szoba.get_tipus()} - Szoba {foglalas.szoba.szobaszam}, Dátum: {foglalas.datum}")
--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-print("Hello Bello")
+from Classes import *
+def main():
+    szalloda = Szalloda("Hotel Python")
+    szalloda.add_szoba(EgyagyasSzoba(101))
+    szalloda.add_szoba(KetagyasSzoba(201))
+    szalloda.add_szoba(KetagyasSzoba(202))
+
+    foglalas_kezelo = FoglalasKezelo()
+
+    foglalas_kezelo.foglalas(szalloda.szobak[0], datetime(2023, 12, 1))
+    foglalas_kezelo.foglalas(szalloda.szobak[1], datetime(2023, 12, 2))
+    foglalas_kezelo.foglalas(szalloda.szobak[2], datetime(2023, 12, 3))
+    foglalas_kezelo.foglalas(szalloda.szobak[0], datetime(2023, 12, 4))
+    foglalas_kezelo.foglalas(szalloda.szobak[1], datetime(2023, 12, 5))
+
+    while True:
+        print("\nVálassz műveletet:")
+        print("1. Foglalás")
+        print("2. Lemondás")
+        print("3. Foglalások listázása")
+        print("4. Kilépés")
+
+        valasztas = input("Választás: ")
+
+        if valasztas == "1":
+            szoba_szam = int(input("Adja meg a szoba számát: "))
+            datum = input("Adja meg a foglalás dátumát (YYYY-MM-DD): ")
+            foglalas_kezelo.foglalas(szalloda.szobak[szoba_szam - 1], datetime.strptime(datum, "%Y-%m-%d"))
+
+        elif valasztas == "2":
+            szoba_szam = int(input("Adja meg a szoba számát: "))
+            datum = input("Adja meg a lemondás dátumát (YYYY-MM-DD): ")
+            foglalas_kezelo.lemondas(szalloda.szobak[szoba_szam - 1], datetime.strptime(datum, "%Y-%m-%d"))
+
+        elif valasztas == "3":
+            foglalas_kezelo.listazas()
+
+        elif valasztas == "4":
+            break
+
+        else:
+            print("Érvénytelen választás.")
+
+if __name__ == "__main__":
+    main()
